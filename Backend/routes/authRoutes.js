15a09be421b9d06b68ea0698e1f87ad629371525
Balaji Patil <balajipatil3252@gmail.com>
2026-06-15 08:47:41 +0530

    feat: implement user management system with backend API routes and frontend administrative dashboard

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { register, login, getMe } from "../controllers/authController.js";
-import { protect } from "../middlewares/authMiddleware.js";
+import { protect, authorizeRoles } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Public Routes
-router.post("/register", register);
 router.post("/login", login);
 
 // Protected Routes (Required JWT Token)
+router.post("/register", protect, authorizeRoles("Super Admin"), register);
 router.get("/me", protect, getMe);
 
 export default router;
