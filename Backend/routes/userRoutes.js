b24d330b058c5aa657f9df1dfe2376409a68baa9
Balaji Patil <balajipatil3252@gmail.com>
2026-06-16 11:17:43 +0530

    feat: add UserManagement page, implement user API routes, and include sample malware test files

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers, updateUserStatus, deleteUser } from "../controllers/authController.js";
+import { getUsers, updateUserStatus, deleteUser, editUser } from "../controllers/authController.js";
 import { protect, authorizeRoles } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -9,7 +9,7 @@ router.use(protect);
 router.use(authorizeRoles("Super Admin"));
 
 router.route("/").get(getUsers);
+router.route("/:id").put(editUser).delete(deleteUser);
 router.route("/:id/status").put(updateUserStatus);
-router.route("/:id").delete(deleteUser);
 
 export default router;
